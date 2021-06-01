26aac8113947615b0ff9c099efaf5f1e5501b89c
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2021-06-01 16:59:32 +0200

    add own styled radio button component

--- surv-eye/src/App.js
+++ surv-eye/src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import './App.css';
+import  RadioButton  from "./RadioButton.js";
+
+
+const App = () => {
+
+  const [selected, setSelected] = useState("0");
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <RadioButton
+        value= "0"
+        selected={selected}
+        text="First Radio Button"
+        onChange={() => setSelected("0")}
+      />
+      <RadioButton
+        value= "1"
+        selected={selected}
+        text="Second Radio Button"
+        onChange={() => setSelected("1")}
+      />
+    </>
   );
 }
 
 export default App;
+
---
+++ surv-eye/src/RadioButton.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import './radiobutton.css';
+
+const RadioButton = ( props ) => {
+
+  return (
+    <div className="radio-container" onClick={() => { props.onChange(props.value) }} >
+      <div className={`radio-outer-circle ${props.value !== props.selected && "unselected"}`}>
+        <div className={`radio-inner-circle ${props.value !== props.selected && "unselected-circle"}`} />
+      </div>
+    </div>
+  );
+}
+
+export default RadioButton; 
