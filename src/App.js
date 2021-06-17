b18740969416d487ac7798f33df74330cdf10950
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2021-06-17 14:49:09 +0200

    Add smoothening and change gaze point style

--- src/App.js
+++ src/App.js
@@ -13,7 +13,11 @@ class WebGazeLoader extends React.Component {
   constructor() {
     super();
     this.state = {
-      context: {x: -1, y: -1}
+      context: {x: -1, y: -1}, 
+      smoothFactor: 10,
+      dataX: 0,
+      dataY: 0,
+      dataPoints: 0
     //         gazeDisplay: "none", // do not display gaze
     //         fixRange: 100, // radius for fixation
     //         fixTime: 2, // dwelltime
@@ -22,20 +26,36 @@ class WebGazeLoader extends React.Component {
     };
   }
 
+
   handleScriptLoad() {
     webgazer.setGazeListener((data, elapsedTime) => {
       if (data == null) {
-          console.log("data null!");
         return;
       }
+      if (this.dataPoints != this.smoothFactor) {
+        var newData = [webgazer.util.bound(data)];
+        this.setState({dataX: this.dataX + newData["x"]});
+        this.setState({dataY: this.dataY + newData["y"]});
+        this.setState({dataPoints: this.dataPoints + 1});
+      } else {
       // Make sure predictions are alway in bounds of the viewport
-      this.setState({context: webgazer.util.bound(data)})
+      var averageX = this.dataX / this.smoothFactor; 
+      var averageY = this.dataY / this.smoothFactor; 
+      var averaged = {x: averageX, y: averageY};
+      this.setState({context: webgazer.util.bound(averaged)})
+      this.setState({dataX: 0});
+      this.setState({dataY: 0});
+      this.setState({dataPoints: 0});
+      }
+
     }).begin();
   }
 
   handleScriptError() {
     console.log('error');
   }
+
+
 
   render() {
     return (
