a2c888aefaa0fe6df8e4806dabd1c4fd39309181
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2021-06-18 14:31:03 +0200

    Add radio button with text

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,25 @@
-
+import React, { useState, useEffect } from 'react';
+import { WebGazeContext } from './WebGazeContext';
+import RadioButton from './RadioButton';
 
 const MainApp =  () => {
+  const [state, setState] = useState("no state");
+ 
 
     return (
-      <div>
-      <p>The main content goes here</p>
+      <WebGazeContext.Consumer>
+      {context => (
+      <div style={{display: "flex"}}>
+
+      <RadioButton id ="Button1"
+      value="0"
+      selected={"selected"}
+      text="First Radio Button"
+      onChange={() => console.log("hi")}/>
+              <p>{"x is " + context.x + " and y is " + context.y}</p>
       </div>
+      )}
+      </WebGazeContext.Consumer>
     );
   }
   
