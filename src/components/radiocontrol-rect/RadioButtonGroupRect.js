d9c4f1a7ee0f21dc5eea1ecf8c1ffc2234302229
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2021-08-20 15:50:20 +0200

    Add visual selection and inspection indicators for gaze interaction

--- src/components/radiocontrol-rect/RadioButtonGroupRect.js
+++ src/components/radiocontrol-rect/RadioButtonGroupRect.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import RadioButtonRect from "./RadioButtonRect.js";
 import { WebGazeContext } from '../../context/WebGazeContext';
 import Ruler from '../ruler/Ruler.js';
 
 import './radiobuttongrouprect.css'
+
 
 const RadioButtonGroup = (props) => {
 
@@ -10,109 +12,40 @@ const RadioButtonGroup = (props) => {
         props.setItemValue(value);
     }
 
-    return (
-        <WebGazeContext.Consumer >
-            {context => (
-                <div id="RULER-CONTAINER">
-                    <Ruler/>
+    const LABELS = ["Strongly agreee", "Agree", "Somewhat agree", "Neutral", "Somewhat disagree", "Disagree", "Strongly Disagree"];
+    const RADIOBUTTONCOUNT = [1, 2, 3, 4, 5, 6, 7];
 
- <div id="RADIO-BUTTON-GROUP-CONTAINER">
-                    <RadioButtonRect
-                        idTarget="radio-target-1"
-                        idFill="radio-fill-1"
-                        value="1"
-                        selected={props.value}
-                        label="Strongly agree"
-                        icon="Strongly agree"
-                        onFixation={onSelection}
-                        context={context}
-                        color="#033f63"
-                    />
+    const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
 
-                    <RadioButtonRect
-                        idTarget="radio-target-2"
-                        idFill="radio-fill-2"
-                        value="2"
-                        selected={props.value}
-                        label="Agree"
-                        icon="Agree"
-                        onFixation={onSelection}
-                        context={context}
-                        color="#033f63"
-                    />
+    const radioButtons = (context) => RADIOBUTTONCOUNT.map((radiobutton) =>
+        <RadioButtonRect
+            idTarget={"radio-target-" + radiobutton}
+            idLabel={"radio-label-" + radiobutton}
+            idFill={"radio-fill-" + radiobutton}
+            value={"" + radiobutton}
+            selected={props.value}
+            label={LABELS[radiobutton - 1]}
+            onFixation={onSelection}
+            context={context}
+        />
+    )
 
-                    <RadioButtonRect
-                        idTarget="radio-target-3"
-                        idFill="radio-fill-3"
-                        value="3"
-                        selected={props.value}
-                        label="Somewhat agree"
-                        icon="Somewhat agree"
-                        onFixation={onSelection}
-                        context={context}
-                        color="#033f63"
+return (
+    <WebGazeContext.Consumer >
+        {context => (
+            <div id="RULER-CONTAINER">
+                <Ruler />
 
-                    />
-
-
-                    <RadioButtonRect
-                        idTarget="radio-target-4"
-                        idFill="radio-fill-4"
-                        value="4"
-                        selected={props.value}
-                        label="Neutral"
-                        icon="Neutral"
-                        onFixation={onSelection}
-                        context={context}
-                        color="#033f63"
-
-                    />
-
-
-                    <RadioButtonRect
-                        idTarget="radio-target-5"
-                        idFill="radio-fill-5"
-                        value="5"
-                        selected={props.value}
-                        label="Somewhat disagree"
-                        icon="Somewhat disagree"
-                        onFixation={onSelection}
-                        context={context}
-                        color="#033f63"
-                    />
-
-                    <RadioButtonRect
-                        idTarget="radio-target-6"
-                        idFill="radio-fill-6"
-                        value="6"
-                        selected={props.value}
-                        label="Disagree"
-                        icon="Disagree"
-                        onFixation={onSelection}
-                        context={context}
-                        color="#033f63"
-                    />
-
-                    <RadioButtonRect
-                        idTarget="radio-target-7"
-                        idFill="radio-fill-7"
-                        value="7"
-                        selected={props.value}
-                        label="Strongly disagree"
-                        icon="Strongly disagree"
-                        onFixation={onSelection}
-                        context={context}
-                        color="#033f63"
-                    />
+                <div id="RADIO-BUTTON-GROUP-CONTAINER">
+                    {radioButtons(context)}
 
                 </div>
-                </div>
-               
+            </div>
 
-            )}
-        </WebGazeContext.Consumer>
+        )}
+    </WebGazeContext.Consumer>
 
-    );
+);
 }
 
 export default RadioButtonGroup;
--- src/components/radiocontrol-rect/RadioButtonRect.js
+++ src/components/radiocontrol-rect/RadioButtonRect.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import './radiobuttonrect.css';
-// import { click } from "../functions/click";
 import { isGazeWithinElement } from '../../functions/isGazeWithinElement';
 import { convertAngleToPx } from "../../functions/convertAngleToPx";
 
@@ -8,7 +7,11 @@ import { convertAngleToPx } from "../../functions/convertAngleToPx";
 const RadioButton = (props) => {
 
   const [radioFillClass, setRadioFillClass] = useState(props.selected === props.value ? "radio-fill" : "radio-fill unselected");
-
+  const [totalTargetClass, setTotalTargetClass] = useState("total-target-area-radio");
+  const [labelClass, setLabelClass] = useState("radio-button-label");
+  const [lastGazeDetected, setLastGazeDetected] = useState(0);
+  const [isInspected, setIsInspected] = useState(false);
+  const INSPECTIONTIME = 1000;
   const MINTARGETSIZE = convertAngleToPx(4.17);
 
   const [eventListener, setEventListener] = useState(false);
@@ -17,43 +20,63 @@ const RadioButton = (props) => {
   // eslint-disable-next-line
   useEffect(() => {
 
-    // If this radio button is currently not selected
-    if (props.selected !== props.value) {
-      if(!eventListener)
-    {
-      document.getElementById(props.idFill).addEventListener("transitionend", onTransitionEnd, false);
-      setEventListener(true);
-    }
+    if (isGazeWithinElement(props.idTarget, 0, props.context.x, props.context.y)) {
+      
+      handleInspection();
+      handleSelection(); 
 
-      if (isGazeWithinElement(props.idTarget, 0, props.context.x, props.context.y)) {
-        setRadioFillClass("radio-fill transitioning");
-        document.getElementById(props.idFill).style.backgroundColor = props.color;
-      } else {
-        setRadioFillClass("radio-fill unselected transitioning");
-        document.getElementById(props.idFill).style.backgroundColor = props.color;
+        // Remove all visual feedback when gaze is not detected within radio button and button is currently not selecgted
+       } else {
+        setTotalTargetClass("total-target-area-radio");
+        setLabelClass("radio-button-label");
+        setIsInspected(false);
+
+        if (props.selected !== props.value) {
+          setRadioFillClass("radio-fill unselected transitioning");
+        }
+
       }
-    } else if (props.selected === props.value && radioFillClass) {
-      setRadioFillClass("radio-fill");
-      document.getElementById(props.idFill).style.backgroundColor = props.color;
-    }
-
-  //   // This line makes the selected element disappear, but it throws an error when switching to slider element 
-  //   return () => {
-
-  //     if(document.getElementById(props.idFill) !== null) {
-  //     console.log(document.getElementById(props.idFill))
-  //     document.getElementById(props.idFill).removeEventListener("transitionend", onTransitionEnd);
-  //   }
-  // }
 
   });
 
+  // Provide visual feedback during inspection of interaction elements
+  const handleInspection = () => {
+    setTotalTargetClass("total-target-area-radio total-target-inspected transitioning");
+    setLabelClass("radio-button-label label-inspected transitioning");
+    // Handle case of radio button being already inspected currently
+    if (!isInspected) {
+      setIsInspected(true);
+      setLastGazeDetected(new Date().getTime());
+  }
+}
+
+// Provide visual feedback when selection process had been triggered
+const handleSelection= () => {
+  if (props.selected !== props.value) {
+    if (!eventListener) {
+      document.getElementById(props.idFill).addEventListener("transitionend", onTransitionEnd, false);
+      setEventListener(true);
+    }
+      let currentTime = new Date().getTime();
+      if (isInspected && currentTime >= lastGazeDetected + INSPECTIONTIME) {
+        setRadioFillClass("radio-fill transitioning");
+        setLastGazeDetected(currentTime);
+      }
+    }  else {
+      if (props.value === props.selected) {
+      setRadioFillClass("radio-fill");
+
+        
+      }
+      
+    }
+}
+
+
   // Handle fixation on scroll button
   const onTransitionEnd = (event) => {
-    // console.log(Math.round(MINTARGETSIZE * 0.9 *0.8));
     if (document.getElementById(props.idFill).offsetHeight > 0 && event.propertyName === "height") {
-        props.onFixation(props.value);
-        // click(props.idFill);
+      props.onFixation(props.value);
 
     }
     document.getElementById(props.idFill).removeEventListener("transitionend", onTransitionEnd);
@@ -67,12 +90,11 @@ const RadioButton = (props) => {
       <div style={{
         minWidth: MINTARGETSIZE
         , minHeight: MINTARGETSIZE
-      }} id={props.idTarget} className="total-target-area-radio">
-            <p className="radio-button-label"> {props.label} </p>
+      }} id={props.idTarget} className={totalTargetClass}>
+        <p id={props.idLabel} className={labelClass}> {props.label} </p>
 
-
-          <div id={props.idFill} className={radioFillClass} >
-            {/* <p style={{ margin: "0", textAlign: "center", fontSize: "32px", color: "white", fontWeight: 800 }}>{props.icon}</p> */}
+        <div id={props.idFill} className={radioFillClass} >
+          {/* <p style={{ margin: "0", textAlign: "center", fontSize: "32px", color: "white", fontWeight: 800 }}>{props.icon}</p> */}
         </div>
       </div>
     </div>
