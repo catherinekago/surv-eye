b137717dd2847e218426198e37b95c9b6f6c8bbc
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2021-09-24 08:36:55 +0200

    Set slider starting position to middle and reset for question switch

--- src/components/slider-2/Slider.js
+++ src/components/slider-2/Slider.js
@@ -13,7 +13,7 @@ import "./slider.css";
 // Where does it have to be added? --> Questionnaire, QuestionnaireItem
 
 const Slider = (props) => {
-    const [sliderID, setSliderID] = useState(props.id);
+    const [sliderID, setSliderID] = useState(null);
     const [isReset, setIsReset] = useState(false);
     const [lastGazeMoveRight, setLastGazeMoveRight] = useState(0);
     const [isRightInpected, setIsRightInspected] = useState(false);
@@ -38,9 +38,10 @@ const Slider = (props) => {
     useEffect(() => {
         if (props.id !== sliderID) {
             if (!isReset) {
-                resetKnob();
+                setTimeout(() => { resetKnob();  }, 200)
+
             } else {
-                document.getElementById("KNOB-SLIDER2").style.transform = "translate(0px)";
+                // document.getElementById("KNOB-SLIDER2").style.transform = "translate(0px)";
                 setSliderID(props.id);
                 setIsReset(false);
             }
@@ -65,31 +66,34 @@ const Slider = (props) => {
 
     // Move knob to middle
     const resetKnob = () => {
-        let KNOB_MIDDLE = document.getElementById("KNOB-SLIDER2").getBoundingClientRect().left + document.getElementById("KNOB-SLIDER2").offsetWidth / 2;
+        let KNOB_POS = document.getElementById("KNOB-SLIDER2").getBoundingClientRect().left ;
+        let KNOB_WIDTH = + document.getElementById("KNOB-SLIDER2").offsetWidth; 
+        console.log("knob pos " + KNOB_POS)
+        console.log("knob width " + KNOB_WIDTH)
         let MIDDLE = document.getElementById("SCALE-SLIDER2").offsetWidth / 2;
-        let TRANSLATE_DIRECTION = KNOB_MIDDLE > 0.05 * window.innerWidth + MIDDLE ? -1 : 1;
-        let translateBy = TRANSLATE_DIRECTION * (KNOB_MIDDLE - (0.05 * window.innerWidth + MIDDLE));
+        console.log("scale middle " + MIDDLE)
+        let translateBy = KNOB_POS > 0.05 * window.innerWidth + MIDDLE - 0.5*KNOB_WIDTH ? Math.round(KNOB_POS - MIDDLE - 0.5*window.innerWidth + 0.5*KNOB_WIDTH) : Math.round(MIDDLE - KNOB_POS);
+        console.log("translate by" + translateBy)
         document.getElementById("KNOB-SLIDER2").style.transform = "translate(" + (translateBy) + "px)"
         setIsReset(true);
     }
 
     // Calculate current value according to scale dimensions and knob position
     const calculateCurrentValue = () => {
-        if (document.getElementById("SCALE-SLIDER2") !== null) {
+        if (props.value === null) {
+            return props.measure === "" ? 50 : props.max / 2;
+        } else if (document.getElementById("SCALE-SLIDER2") !== null) {
             let SCALE_WIDTH = document.getElementById("SCALE-SLIDER2").offsetWidth;
             let SCALE_UNIT = props.measure === "" ? SCALE_WIDTH / 100 : SCALE_WIDTH / props.max;
 
-            let positionOnScale = document.getElementById("KNOB-SLIDER2").getBoundingClientRect().left - window.innerWidth * 0.05;
+            let positionOnScale = document.getElementById("KNOB-SLIDER2").getBoundingClientRect().left + document.getElementById("KNOB-SLIDER2").offsetWidth / 2 - window.innerWidth*0.05;
             let value = Math.round((positionOnScale) / SCALE_UNIT);
             return value;
-        } else {
-            if (props.value === null) {
-                return props.measure === "" ? 50 : props.max / 2;
-            } else {
-                return props.value;
-            }
-
         }
+    }
+
+    const translateKnob = () => {
+        return 0;
     }
 
 
@@ -99,43 +103,43 @@ const Slider = (props) => {
             <div id="MOVE-BUTTON-AREA">
                 <div id="MOVE-BUTTON-LEFT" style={{ width: document.getElementById("KNOB-SLIDER2") !== null ? (document.getElementById("KNOB-SLIDER2").getBoundingClientRect().left + document.getElementById("KNOB-SLIDER2").offsetWidth / 2 - window.innerWidth * 0.05) + "px" : "0px" }}>
                     <div id="ARROW-LEFT-CONTAINER" className="arrow-container">
-                                                <div id="ARROW-LEFT-INDICATOR" className={leftArrowIndicatorClass}></div>
+                        <div id="ARROW-LEFT-INDICATOR" className={leftArrowIndicatorClass}></div>
                         <img id="ARROW-LEFT" className="arrow-img" src={ArrowLeft} alt="Slider Arrow Left" />
                     </div>
                 </div>
 
-                <div id="MOVE-BUTTON-RIGHT" style={{ width: document.getElementById("KNOB-SLIDER2") !== null ? (window.innerWidth - document.getElementById("KNOB-SLIDER2").getBoundingClientRect().left + document.getElementById("KNOB-SLIDER2").offsetWidth / 2 - window.innerWidth * 0.05) + "px" : "0px" }}>
+                <div id="MOVE-BUTTON-RIGHT" style={{ width: document.getElementById("KNOB-SLIDER2") !== null ? (window.innerWidth - document.getElementById("KNOB-SLIDER2").getBoundingClientRect().right + document.getElementById("KNOB-SLIDER2").offsetWidth / 2 - window.innerWidth * 0.05) + "px" : "0px" }}>
                     <div id="ARROW-RIGHT-CONTAINER" className="arrow-container">
                         <div id="ARROW-RIGHT-INDICATOR" className={rightArrowIndicatorClass}></div>
-                            <img id="ARROW-RIGHT" className="arrow-img" src={ArrowRight} alt="Slider Arrow Right" />
-                        </div>
-
+                        <img id="ARROW-RIGHT" className="arrow-img" src={ArrowRight} alt="Slider Arrow Right" />
                     </div>
+
                 </div>
+            </div>
 
-                {/* <div id="SCALE-COMPONENT-SLIDER2" style={{ marginTop: "75px", width: "90%" }}>
-                <div id={"KNOB-SLIDER2"} style={{ transform: "translate(" + translateInteractionKnob() + "px)" }}>
-
-
-                    {props.measure === "" ? null : <p id={"KNOB-SLIDER2-LABEL"}>{calculateCurrentValue() + props.measure}</p>}
+            <div id="SCALE-COMPONENT-SLIDER2">               
+             <div id={"SCALE-SLIDER2"}></div>
+                <div id={"KNOB-SLIDER2"} style={{ transform: "translate(" + translateKnob() + "px)" }}>
+                    {props.measure === "" ? null : <p id={"KNOB-SLIDER2-LABEL"}>{ calculateCurrentValue() + props.measure}</p>}
+                    
                 </div>
                 <p id="SCALE-SLIDER2-MIN">{props.min + props.measure}</p>
-                <div id={"SCALE-SLIDER2"}></div>
                 <p id="SCALE-SLIDER2-MAX">{props.max + props.measure}</p>
+
 
             </div>
 
-            <div id="STOP-COMPONENT">
-                <div id="STOP-MARKER">
+            <div id="STOP-COMPONENT"></div> 
+                {/* <div id="STOP-MARKER">
                     {props.measure === "" ? null : <p id={"STOP-MARKER-LABEL"}>{calculateCurrentValue() + props.measure}</p>}
                 </div>
                 <div id="STOP-GAZE-INDICATOR" className={stopAreaSelectionClass}></div>
-            </div> */}
+            </div>  */}
 
 
 
-            </div>
-            );
+        </div>
+    );
 }
 
-            export default Slider;
+export default Slider;
