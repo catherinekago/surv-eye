dd72ded214191f9ebf63e0d0b5f991e199a61c7f
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2021-09-30 08:31:54 +0200

    Make slider movement dependent on time intervals

--- src/components/slider-1/Slider.js
+++ src/components/slider-1/Slider.js
@@ -1,6 +1,6 @@
 import { isGazeWithinElement } from '../../functions/isGazeWithinElement';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import "./slider.css";
 
 // How to make a questionnaire item for this slider: 
@@ -21,6 +21,10 @@ const Slider = (props) => {
     const [isSideInspected, setIsSideInspected] = useState(false);
     const INSPECTIONTIME = 500;
     const [isKnobTransitioning, setIsKnobTransitioning] = useState(false);
+    const [movement, setMovement] = useState(null); // right and left set in handleGazeWithinDirectionButtons
+    const movementRef = useRef(movement);
+    movementRef.current = movement;
+    const [translateKnobTo, setTranslateKnobTo] = useState(0);
 
     useEffect(() => {
         if (!props.isInspectionArea) {
@@ -37,18 +41,31 @@ const Slider = (props) => {
             if (props.value === calculateCurrentValue()) {
                 handleInteractionUnlock();
             } else {
+                handleGazeWithingMoveButtons();
                 handleGazeWithinKnobArea();
             }
         }
     });
 
+    // Transate knob position if movement is triggered
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // Determine movement only if knob is not locked or in the process of being locked
+            if (movement !== "none") {
+                translateInteractionKnob();
+            }
+        }, 5);
+
+        return () => clearInterval(interval);
+    }, [])
+
     // Move interaction knob to min value
     const resetKnob = () => {
-        let KNOB_POS = document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left ;
+        let KNOB_POS = document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left;
         console.log("left is at " + KNOB_POS)
-        let translateBy = KNOB_POS  - 0.05* window.innerWidth;
+        let translateBy = KNOB_POS - 0.05 * window.innerWidth;
         console.log("reset Knob by " + translateBy);
-        document.getElementById("KNOB-AREA-INTERACTION").style.transform = "translate(" + (-1* translateBy) + "px)"
+        document.getElementById("KNOB-AREA-INTERACTION").style.transform = "translate(" + (-1 * translateBy) + "px)"
         setIsReset(true);
         // document.getElementById("INTERACTION-AREA-LEFT").style.offsetWidth = "0px";
         // document.getElementById("INTERACTION-AREA-RIGHT").style.offsetWidth = (window.innerWidth - document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().right) + "px";
@@ -72,6 +89,7 @@ const Slider = (props) => {
                     setIsKnobTransitioning(true);
                     setLastTransitionStart(currentTime);
                     setLastGazeSelection(currentTime);
+                    setMovement("none");
 
                 }
                 // Trigger selection once transition has completed 
@@ -100,15 +118,14 @@ const Slider = (props) => {
         }
     }
 
-    // Unlock interaction area left and right of dwell time has passed
+    // Unlock interaction area left and right once dwell time has passed
     const handleInteractionUnlock = () => {
         if (props.value === calculateCurrentValue()) {
             let currentTime = new Date().getTime();
             // Perform unlock if gaze has been detected within one of the interaction sides
-            if (isGazeWithinElement("INTERACTION-AREA-LEFT", 0, props.context.x, props.context.y) || isGazeWithinElement("INTERACTION-AREA-RIGHT", 0, props.context.x, props.context.y) || (props.context.x < window.innerWidth*0.05 &&  props.context.y >= document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().top) || (props.context.x > window.innerWidth - window.innerWidth*0.05 &&  props.context.y >= document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().top)) {
+            if (isGazeWithinElement("INTERACTION-AREA-LEFT", 0, props.context.x, props.context.y) || isGazeWithinElement("INTERACTION-AREA-RIGHT", 0, props.context.x, props.context.y) || (props.context.x < window.innerWidth * 0.05 && props.context.y >= document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().top) || (props.context.x > window.innerWidth - window.innerWidth * 0.05 && props.context.y >= document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().top)) {
                 // Start delay timer if side is currently not being inspected
                 if (!isSideInspected) {
-                    console.log("inspect")
                     setIsSideInspected(true);
                     setLastGazeSelection(currentTime);
                 }
@@ -119,32 +136,12 @@ const Slider = (props) => {
                     setLastTransitionStart(currentTime);
                     setKnobAreaSelectionClass("knob-area-no-fill knob-transitioning");
                 }
-                // Unlock knob once transformation has been completed by translating the knob in the corresponding direction
+                // Reset knob appearance once transition is completed
                 if (isKnobTransitioning && currentTime >= lastTransitionStart + 800) {
                     setKnobAreaSelectionClass("knob-area-no-fill");
                     setIsKnobTransitioning(false);
                     setIsSideInspected(false);
-
-                    // Translate knob: 
-                    let SCALE_WIDTH = document.getElementById("SCALE-INTERACTION").offsetWidth;
-                    let SCALE_UNIT = props.measure === "" ? SCALE_WIDTH / 100 : SCALE_WIDTH / props.max;
-                    let translateBy = 0;
-                    if (isGazeWithinElement("INTERACTION-AREA-LEFT", 0, props.context.x, props.context.y) || (props.context.x < window.innerWidth*0.05 &&  props.context.y >= document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().top)) {
-                        if (document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left < SCALE_UNIT) {
-                            translateBy = -1 * document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left
-                        } else {
-                            translateBy = (-1 * SCALE_UNIT);
-                        }
-                    } else if (isGazeWithinElement("INTERACTION-AREA-RIGHT", 0, props.context.x, props.context.y) || (props.context.x > window.innerWidth - window.innerWidth*0.05 &&  props.context.y >= document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().top)) {
-                        if (window.innerWidth - document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().right < SCALE_UNIT) {
-                            translateBy = (window.innerWidth - document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left)
-                        } else {
-                            translateBy = SCALE_UNIT;
-                        }
-
-                    }
-                    document.getElementById("KNOB-AREA-INTERACTION").style.transform = "translate(" + translateBy + "px)"
-
+                    handleGazeWithingMoveButtons();
                 }
 
             } else {
@@ -162,7 +159,7 @@ const Slider = (props) => {
             let SCALE_WIDTH = document.getElementById("SCALE-INTERACTION").offsetWidth;
             let SCALE_UNIT = props.measure === "" ? SCALE_WIDTH / 100 : SCALE_WIDTH / props.max;
 
-            let positionOnScale = document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left - window.innerWidth*0.05;
+            let positionOnScale = document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left - window.innerWidth * 0.05;
             let value = Math.round((positionOnScale) / SCALE_UNIT);
             return value;
         } else {
@@ -175,87 +172,94 @@ const Slider = (props) => {
         }
     }
 
-    // Calculate position of knob within interaction area : only move if knob has not been blocked!
-    const translateInteractionKnob = () => {
-        if (document.getElementById("INTERACTION-AREA-LEFT") !== null || document.getElementById("INTERACTION-AREA-RIGHT") !== null) {
-            if (calculateCurrentValue() !== props.value) {
-                let SCALE_WIDTH = document.getElementById("SCALE-INTERACTION").offsetWidth;
-                let SCALE_UNIT = props.measure === "" ? SCALE_WIDTH / 100 : SCALE_WIDTH / props.max;
-                // If gaze has been detected in left area, move to left by one unit
-                if (isGazeWithinElement("INTERACTION-AREA-LEFT", 0, props.context.x, props.context.y) || (props.context.x < window.innerWidth*0.05 &&  props.context.y >= document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().top)) {
-                    if (document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left - window.innerWidth*0.05 <= 0) {
-                        return 0;
-                } else if (document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left - window.innerWidth*0.05 < SCALE_UNIT) {
-                        return (-1 * (document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left - window.innerWidth*0.05 ))
+    // Determine if gaze is detected within gaze buttons and set movement variable accordingly
+    const handleGazeWithingMoveButtons = () => {
+
+        // If gaze has been detected in left area, move to left by one unit
+        if (isGazeWithinElement("INTERACTION-AREA-LEFT", 0, props.context.x, props.context.y) || (props.context.x < window.innerWidth * 0.05 && props.context.y >= document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().top)) {
+            setMovement("left");
+        } else if (isGazeWithinElement("INTERACTION-AREA-RIGHT", 0, props.context.x, props.context.y) || (props.context.x > window.innerWidth - window.innerWidth * 0.05 && props.context.y >= document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().top)) {
+            setMovement("right");
+        } else {
+            setMovement("none");
+        }
+    }
+
+        // Calculate position of knob within interaction area : only move if knob has not been blocked!
+        const translateInteractionKnob = () => {
+            if (movementRef.current === "left" && document.getElementById("INTERACTION-AREA-LEFT") !== null) {
+                // If border has been reached, stop movement
+                    if (document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left - window.innerWidth * 0.05 <= 0) {
+                        setTranslateKnobTo(0);
+                        setMovement("none");
+                        // If remaining space is smaller than 1, move by that available space
+                    } else if (document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left - window.innerWidth * 0.05 < 1) {
+                        setTranslateKnobTo(prev => prev - (-1 * (document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left - window.innerWidth * 0.05)));
+                    // Move by 1 otherwise
                     } else {
-                        return (-1 * SCALE_UNIT);
+                        setTranslateKnobTo(prev => prev - 1);
                     }
-                } else if (isGazeWithinElement("INTERACTION-AREA-RIGHT", 0, props.context.x, props.context.y) || (props.context.x > window.innerWidth - window.innerWidth*0.05 &&  props.context.y >= document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().top)) {
-                    // Else if gaze has been detected in right area, move to right by one unit
-                    if (window.innerWidth - window.innerWidth*0.05 - document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().right  <= 0 ) {
-                        return 0; 
-                    } else if (window.innerWidth - window.innerWidth*0.05 - document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().right < SCALE_UNIT) {
-                        return (window.innerWidth - window.innerWidth*0.05 - document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().right)
+                } else if (movementRef.current === "right" && document.getElementById("INTERACTION-AREA-RIGHT") !== null) {
+                    if (window.innerWidth - window.innerWidth * 0.05 - document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().right <= 0) {
+                        setTranslateKnobTo(0);
+                        setMovement("none");
+                    } else if (window.innerWidth - window.innerWidth * 0.05 - document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().right < SCALE_UNIT) {
+                        setTranslateKnobTo(prev => prev + (window.innerWidth - window.innerWidth * 0.05 - document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().right)); 
                     } else {
-                        return SCALE_UNIT;
+                        setTranslateKnobTo(prev => prev + 1);
                     }
-                } else {
-                    return 0;
                 }
             }
 
+        // Calculate position of knob within inspection area according to size of scale and given range
+        const translateInspectionKnob = () => {
+            if (document.getElementById("KNOB-AREA-INSPECTION") !== null && document.getElementById("SCALE-INSPECTION") !== null) {
+                let SCALE_WIDTH = document.getElementById("SCALE-INSPECTION").offsetWidth;
+                let SCALE_UNIT = props.measure === "" ? SCALE_WIDTH / 100 : SCALE_WIDTH / props.max;
+                let KNOB_AREA_WIDTH = document.getElementById("KNOB-AREA-INSPECTION").offsetWidth;
+                let MARGIN_LEFT = (window.innerWidth - SCALE_WIDTH) / 2;
+                let newPosition = (SCALE_UNIT * props.value - 0.5 * KNOB_AREA_WIDTH);
+                return (newPosition);
+            }
+
         }
-    }
-
-    // Calculate position of knob within inspection area according to size of scale and given range
-    const translateInspectionKnob = () => {
-        if (document.getElementById("KNOB-AREA-INSPECTION") !== null && document.getElementById("SCALE-INSPECTION") !== null) {
-            let SCALE_WIDTH = document.getElementById("SCALE-INSPECTION").offsetWidth;
-            let SCALE_UNIT = props.measure === "" ? SCALE_WIDTH / 100 : SCALE_WIDTH / props.max;
-            let KNOB_AREA_WIDTH = document.getElementById("KNOB-AREA-INSPECTION").offsetWidth;
-            let MARGIN_LEFT = (window.innerWidth - SCALE_WIDTH) / 2;
-            let newPosition = (SCALE_UNIT * props.value - 0.5*KNOB_AREA_WIDTH);
-            return (newPosition);
-        }
-
-    }
 
 
-    return (
+        return (
 
-        <div id="SLIDER-COMPONENT">
+            <div id="SLIDER-COMPONENT">
 
-            <div id="INTERACTION-AREA">
-                {!props.isInspectionArea ? <div id="INTERACTION-AREA-LEFT" style={{ width: document.getElementById("KNOB-AREA-INTERACTION") !== null ? (document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left - window.innerWidth*0.05) + "px" : "0px" }}> </div> : null}
+                <div id="INTERACTION-AREA">
+                    {!props.isInspectionArea ? <div id="INTERACTION-AREA-LEFT" style={{ width: document.getElementById("KNOB-AREA-INTERACTION") !== null ? (document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().left - window.innerWidth * 0.05) + "px" : "0px" }}> </div> : null}
 
 
-                <div id={!props.isInspectionArea ? "KNOB-AREA-INTERACTION" : "KNOB-AREA-INSPECTION"} style={{ transform: !props.isInspectionArea ? "translate(" + translateInteractionKnob() + "px)" : "translate(" + translateInspectionKnob() + "px)" }}>
-                    <div id="KNOB-AREA-SELECTION" className={!props.isInspectionArea ? knobAreaSelectionClass : ""}> </div>
-                    <div id={!props.isInspectionArea ? "KNOB-INTERACTION" : "KNOB-INSPECTION"}>
+                    <div id={!props.isInspectionArea ? "KNOB-AREA-INTERACTION" : "KNOB-AREA-INSPECTION"} style={{ transform: !props.isInspectionArea ? "translate(" + translateKnobTo + "px)" : "translate(" + translateInspectionKnob() + "px)" }}>
+                        <div id="KNOB-AREA-SELECTION" className={!props.isInspectionArea ? knobAreaSelectionClass : ""}> </div>
+                        <div id={!props.isInspectionArea ? "KNOB-INTERACTION" : "KNOB-INSPECTION"}>
+                        </div>
+                        {props.measure === "" ? null : <p id={!props.isInspectionArea ? "KNOB-INTERACTION-LABEL" : "KNOB-INSPECTION-LABEL"}>{calculateCurrentValue() + props.measure}</p>}
                     </div>
-                    {props.measure === "" ? null : <p id={!props.isInspectionArea ? "KNOB-INTERACTION-LABEL" : "KNOB-INSPECTION-LABEL"}>{calculateCurrentValue() + props.measure}</p>}
+                    {!props.isInspectionArea ? <div id="INTERACTION-AREA-RIGHT" style={{ width: document.getElementById("KNOB-AREA-INTERACTION") !== null ? (window.innerWidth - document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().right - window.innerWidth * 0.05) + "px" : "0px" }}></div> : null}
+
+
                 </div>
-                {!props.isInspectionArea ? <div id="INTERACTION-AREA-RIGHT" style={{ width: document.getElementById("KNOB-AREA-INTERACTION") !== null ? (window.innerWidth - document.getElementById("KNOB-AREA-INTERACTION").getBoundingClientRect().right - window.innerWidth*0.05) + "px" : "0px" }}></div> : null}
+
+                <div id="SCALE-COMPONENT" style={{ marginTop: !props.isInspectionArea ? "2px" : "75px", width: !props.isInspectionArea ? "72%" : "90%" }}>
+
+                    <p id="SCALE-MIN">{props.min + props.measure}</p>
+
+                    <div id={!props.isInspectionArea ? "SCALE-INTERACTION" : "SCALE-INSPECTION"}>
+                    </div>
+
+                    <p id="SCALE-MAX">{props.max + props.measure}</p>
+
+                </div>
 
 
             </div>
 
-            <div id="SCALE-COMPONENT" style={{ marginTop: !props.isInspectionArea ? "2px" : "75px", width: !props.isInspectionArea ? "72%" : "90%" }}>
 
-                <p id="SCALE-MIN">{props.min + props.measure}</p>
+        );
+    }
 
-                <div id={!props.isInspectionArea ? "SCALE-INTERACTION" : "SCALE-INSPECTION"}>
-                </div>
-
-                <p id="SCALE-MAX">{props.max + props.measure}</p>
-
-            </div>
-
-
-        </div>
-
-
-    );
-}
-
-export default Slider;
+    export default Slider;
